d272cc7a8be19d09fef4928c09230186959c7bd9
Jesper Carlsson <jespercarlsson@Jespers-MacBook-Air.local>
2017-11-15 15:47:47 +0100

    Lab2 is kind of okay

--- Labs/skeleton/code/curlscript.sh
+++ Labs/skeleton/code/curlscript.sh
@@ -1,4 +1,23 @@
 curl -d "entry=CURLZ" -X POST 10.1.0.1:80/board
-curl -d "entry=Hej" -X POST 10.1.0.5:80/board
-curl -d "entry=HelloTwo" -X POST 10.1.0.4:80/board
+curl -d "entry=Hej" -X POST 10.1.0.2:80/board
+curl -d "entry=HelloTwo" -X POST 10.1.0.3:80/board
+curl -d "entry=Hellyio1" -X POST 10.1.0.4:80/board
+curl -d "entry=Helly2o2" -X POST 10.1.0.1:80/board
+curl -d "entry=Hellyoo3" -X POST 10.1.0.2:80/board
+curl -d "entry=Hellyoo4" -X POST 10.1.0.3:80/board
+curl -d "entry=Hellyoo5" -X POST 10.1.0.4:80/board
+curl -d "entry=Hellyoo6" -X POST 10.1.0.1:80/board
+curl -d "entry=Hellyoo7" -X POST 10.1.0.2:80/board
+curl -d "entry=Hellyoo8" -X POST 10.1.0.3:80/board
+curl -d "entry=Hellyoo9" -X POST 10.1.0.4:80/board
+curl -d "entry=Hellyoo10" -X POST 10.1.0.1:80/board
+curl -d "entry=Hellyoo11" -X POST 10.1.0.2:80/board
+curl -d "entry=Hellyoo12" -X POST 10.1.0.2:80/board
+curl -d "entry=Hellyoo13" -X POST 10.1.0.3:80/board
+curl -d "entry=Hellyoo14" -X POST 10.1.0.3:80/board
+curl -d "entry=Hellyoo15" -X POST 10.1.0.3:80/board
+curl -d "entry=Hellyoo16" -X POST 10.1.0.4:80/board
+curl -d "entry=Hellyoo17" -X POST 10.1.0.1:80/board
+curl -d "entry=Hellyoo18" -X POST 10.1.0.4:80/board
+curl -d "entry=Hellyoo19" -X POST 10.1.0.2:80/board
 
